269c9f80fdc295c146f54a8cf1d16d2a1c819660
CharithDombawala <2000charith@gmail.com>
2025-07-09 17:05:31 +0530

    Implement inventory management features with CRUD operations and ModelMapper integration

--- inventory/src/main/java/com/example/inventory/repo/InventoryRepo.java
+++ inventory/src/main/java/com/example/inventory/repo/InventoryRepo.java
@@ -1,4 +1,12 @@
 package com.example.inventory.repo;
 
-public interface InventoryRepo {
+import com.example.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface InventoryRepo extends JpaRepository<Inventory, Integer> {
+    @Query(value = "SELECT * FROM inventory WHERE item_id = ?1", nativeQuery = true)
+    Inventory getItemById(Integer itemId);
 }
